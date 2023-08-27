aa424ef470786524f0a30ba074c6c35c4a49e86d
dulap16 <tudorbrk8@yahoo.com>
2023-08-27 22:52:08 +0300

    Add Lerp() method

--- Lerper.cs
+++ Lerper.cs
@@ -12,5 +12,12 @@ namespace Assets.SCRIPTS.Start_Page
         [SerializeField] [Range(0, 10)] public float speed;
         [SerializeField] [Range(0, 20)] public float delay;
 
+        public void Lerp()
+        {
+            _current = Mathf.MoveTowards(_current, _target, speed * Time.deltaTime);
+            if (_target - _current > -0.001f && _target - _current < 0.001f)
+                _current = _target;
+        }
+
     }
 }
