cf299cbe43d2f2258c43cf087a70b7a60bb507bd
dulap16 <tudorbrk8@yahoo.com>
2023-08-27 22:52:50 +0300

    Add variables for position, color, and rotation
    Will need to add scaling too

--- ObjectToBeLerped.cs
+++ ObjectToBeLerped.cs
@@ -6,5 +6,35 @@ namespace Assets.SCRIPTS.Start_Page
 {
     public class ObjectToBeLerped : MonoBehaviour
     {
+        private GameObject go;
+        // WHAT IS THE OBJECT LERPING
+        [Header("Lerped Properties")]
+        [SerializeField] private bool position = false;
+        [SerializeField] private bool color = false;
+        [SerializeField] private bool rotation = false;
+
+
+        [Header("Position lerping")]
+        [SerializeField] public bool inheritInitPos = true;
+        [SerializeField] public Vector3 initPos;
+        [SerializeField] public Vector3 finalPos;
+        [SerializeField] private Lerper positionLerper; 
+        [SerializeField] private AnimationCurve _posCurve;
+
+        [Header("Color lerping")]
+        [SerializeField] private bool inheritInitColor = true;
+        [SerializeField] private Color initColor;
+        [SerializeField] private Color finalColor;
+        [SerializeField] private Lerper colorLerper;
+        [SerializeField] private AnimationCurve _colorCurve;
+
+
+        [Header("Rotation lerping")]
+        [SerializeField] private bool inheritInitRot = true;
+        [SerializeField] private Quaternion initRot;
+        [SerializeField] private Quaternion finalRot;
+        [SerializeField] private Lerper rotationLerper;
+        [SerializeField] private AnimationCurve _rotCurve;
+
     }
 }
