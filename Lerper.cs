0977071d0013e8c10c6b5f70e1f647d328bb04d8
dulap16 <tudorbrk8@yahoo.com>
2023-08-27 22:51:52 +0300

    Add variables

--- Lerper.cs
+++ Lerper.cs
@@ -7,5 +7,10 @@ namespace Assets.SCRIPTS.Start_Page
     [System.Serializable]
     public class Lerper
     {
+        private float _current = 0;
+        private float _target = 0;
+        [SerializeField] [Range(0, 10)] public float speed;
+        [SerializeField] [Range(0, 20)] public float delay;
+
     }
 }
