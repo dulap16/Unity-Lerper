997bd2df0db760dfa3910272010ace73fcf948a4
dulap16 <tudorbrk8@yahoo.com>
2023-08-28 23:50:11 +0300

    Add variables and serialize everything

--- Stage.cs
+++ Stage.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 
 namespace Assets.SCRIPTS.Start_Page
 {
+    [Serializable]
     public class Stage
     {
+        [SerializeField] [Range(0, 10)] private float _delay, _speed;
+        [SerializeField] private bool _inheritLast;
+        [SerializeField] private Vector3 initPos, finalPos;
+        [SerializeField] private Vector3 initScale, finalScale;
+        [SerializeField] private Color initColor, finalColor;
+        [SerializeField] private Quaternion initRot, finalRot;
+        [SerializeField] private List<AnimationCurve> _curves; // 1 - pos, 2 -scale, 3 - color, 4 - rotation
     }
 }
